fac1e37131e9159ea41fb33fcddab5cc080d12f4
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2019-11-21 21:50:52 -0500

    Designed top-nav and left side-bar

---
+++ includes/keydatabase_footer.php
@@ -0,0 +1,10 @@
+</div>
+	<!-- END WRAPPER -->
+	<!-- Javascript -->
+	<script src="assets/vendor/jquery/jquery.min.js"></script>
+	<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="assets/scripts/klorofil-common.js"></script>
+	
+</body>
+
+</html>
